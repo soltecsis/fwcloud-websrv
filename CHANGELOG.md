3bbbe3f2f00e3c80eec29c87543132106af0424c
cmunyoz <cmunyoz@soltecsis.com>
2025-09-08 17:19:15 +0200

    Release 2.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,15 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [2.0.2] - 2025-09-08
+### Fixed
+- Updated Node.js packages to the latest versions.
+- Some linter errors.
+
+### Added
+- Forward `X-Forward-*` headers to the API backend.
+
+
 ## [2.0.1] - 2024-10-21
 ### Fixed
 - Updated Node.js packages to the latest versions.
--- src/logs/logs.service.ts
+++ src/logs/logs.service.ts
@@ -1,5 +1,5 @@
 /*
-    Copyright 2020 SOLTECSIS SOLUCIONES TECNOLOGICAS, SLU
+    Copyright 2025 SOLTECSIS SOLUCIONES TECNOLOGICAS, SLU
     https://soltecsis.com
     info@soltecsis.com
 
@@ -44,6 +44,7 @@ export class LogsService {
         }),
         winston.format.printf(
           (info) =>
+            // eslint-disable-next-line @typescript-eslint/no-base-to-string, @typescript-eslint/restrict-template-expressions
             `${info.timestamp}|${info.level.toUpperCase()}|${info.message}`,
         ),
       ),
@@ -65,6 +66,7 @@ export class LogsService {
         winston.format.timestamp({
           format: 'YYYY-MM-DD HH:mm:ss',
         }),
+        // eslint-disable-next-line @typescript-eslint/no-base-to-string, @typescript-eslint/restrict-template-expressions
         winston.format.printf((info) => `${info.timestamp}|${info.message}`),
       ),
       defaultMeta: { service: 'user-service' },
